d56754014b192448c4b97bda7a03d7fa242259cf
viruscamp <viruscamp@gmail.com>
2021-04-23 13:52:53 +0800

    + http-server-socket2 试图用 socket2 库实现优雅关闭

---
+++ http-server-socket2/src/main.rs
@@ -0,0 +1,188 @@
+mod thread_pool;
+use thread_pool::ThreadPool;
+use std::thread;
+use std::io::{Error, ErrorKind};
+use std::time::Duration;
+use std::path::Path;
+use std::sync::mpsc;
+use std::fs::File;
+use std::sync::Arc;
+use std::net::{IpAddr, SocketAddr, TcpStream, Shutdown};
+use socket2::{Socket, Domain, Type};
+use std::io::{BufReader, Read, Write, BufRead};
+
+use thread::spawn;
+use thread::sleep;
+
+fn main() -> Result<(), Error> {
+    println!("http-server using socket2 and threadpool starting");
+
+    let (tx, rx) = mpsc::channel::<DispatchMessage>();
+
+    let port = 20086;
+    let tx1 = tx.clone();
+
+    let (socket, accept_loop) = match create_socket2("127.0.0.1", port) {
+        Ok(socket) => {
+            let socket = Arc::new(socket);
+            let socket1 = socket.clone();
+            let accept_loop = spawn(move || {
+                loop {
+                    let connected = socket.accept();
+                    match connected {
+                        Ok((socket, _)) => {
+                            tx1.send(DispatchMessage::Connected(socket.into())).unwrap_or_default();
+                        }
+                        Err(_) => {
+                             /* connection failed */ 
+                             break;
+                        }
+                    }
+                }
+                println!("accept_loop ends");
+            });
+            (socket1, accept_loop)
+        }
+        Err(err) => {
+            println!("http-server starting failed");
+            //tx1.send(DispatchMessage::Quit).unwrap(); // useless
+            return Err(err);
+        }
+    };
+
+    let mut tp = ThreadPool::new(2);
+    // dispatch loop
+    while let Ok(dispatch_message) = rx.recv() {
+        match dispatch_message {
+            DispatchMessage::Connected(stream) => {
+                let tx = tx.clone();
+                let job = move || {
+                    match handle_connection(stream) {
+                        Ok(RequestResult::Quit) => {
+                            tx.send(DispatchMessage::Quit).unwrap_or_default();
+                        }
+                        Err(err) => {
+                            println!("error: {}", err);
+                        }
+                        _ => {}
+                    }
+                };
+                tp.execute(job);
+                //spawn(job);
+            }
+            DispatchMessage::Start => {}
+            DispatchMessage::Quit => {
+                break;
+            }
+        }
+    }
+
+    println!("try shutdown");
+    socket.shutdown(Shutdown::Both);
+    println!("try join");
+    accept_loop.join();
+
+    println!("http-server quitting");
+    Ok(())
+}
+
+fn create_socket2(ip: &str, port: u16) -> Result<Socket, Error> {
+    let socket = Socket::new(Domain::IPV4, Type::STREAM, None)?;
+    let ip: IpAddr = ip.parse().unwrap();
+    let address = SocketAddr::new(ip, port);
+    let address = address.into();
+    socket.bind(&address)?;
+    socket.listen(128)?;
+
+    Ok(socket)
+}
+
+#[derive(Debug)]
+enum DispatchMessage {
+    Connected(TcpStream),
+    Quit,
+    Start,
+}
+
+#[derive(Debug)]
+enum RequestResult {
+    Ok,
+    Quit,
+}
+
+fn handle_connection(mut stream: TcpStream) -> Result<RequestResult, Error> {
+    let mut ins = BufReader::new(&stream);
+    let mut str = String::new();
+    ins.read_line(&mut str)?;
+
+    let strsubs: Vec<_> = str.split(" ").collect();
+    if strsubs.len() < 3 {
+        return Err(Error::new(ErrorKind::InvalidInput, "http request format error"));
+    }
+    let method = strsubs[0];
+    let uri = strsubs[1];
+
+    println!("method: {} uri: {}", method, uri);
+
+    let (path, query) = match uri.split_once("?") {
+        Some(a) => a,
+        None => (uri, ""),
+    };
+
+    if query == "sleep" {
+        let now = std::time::Instant::now();
+        sleep(Duration::new(20, 0));
+        println!("sleep for {} seconds", now.elapsed().as_secs());
+    }
+
+    if path == "/" {
+        write!(stream, r#"HTTP/1.1 200 OK
+
+<html>
+<body>
+<h1>Welcom to {}</h1>
+</body>
+</html>"#, uri)?;
+    } else {
+        let root = Path::new("E:\\");
+        let fullpath = match path.strip_prefix("/") {
+            Some(p) => root.join(p),
+            None => root.join(path),
+        };
+        match File::open(&fullpath) {
+            Ok(mut f) => {
+                println!("File {:?} opened", fullpath);
+                write!(stream, "HTTP/1.1 200 OK\n\n")?;
+                stream_copy(&mut f, &mut stream)?;
+            }
+            Err(err) => {
+                write!(stream, r#"HTTP/1.1 404 NOT FOUND
+
+<html>
+<body>
+<h1>not found: {}</h1>
+<span>{}</span>
+</body>
+</html>"#, uri, err)?;
+            }
+        }
+    }
+    stream.flush()?;
+
+    if query == "quit" {
+        return Ok(RequestResult::Quit);
+    }
+    return Ok(RequestResult::Ok);
+}
+
+fn stream_copy(r: &mut impl Read, w: &mut impl Write) -> Result<usize, Error> {
+    let mut buf = [0u8; 1024];
+    let mut sz: usize = 0;
+    return loop {
+        let len = r.read(&mut buf)?;
+        //if len == 0 { break Ok(sz) } // 每次都会多读一个 0
+        w.write(&buf[..len])?;
+        sz += len;
+        if len < buf.len() { break Ok(sz) } // 大部分情况 ok , 输入长度 = buflen*n 时 多读多写一个 0
+    }
+}
---
+++ http-server-socket2/src/thread_pool.rs
@@ -0,0 +1,111 @@
+use std::{sync::{Arc, Mutex, mpsc::{Receiver, Sender, channel}}, thread};
+
+pub struct ThreadPool {
+    pool: Vec<Worker>,
+    tx: Sender<JobMessage>,
+    rx: Arc<Mutex<Receiver<JobMessage>>>,
+}
+
+impl ThreadPool {
+    pub fn new(size: usize) -> ThreadPool {
+        let mut pool = Vec::with_capacity(size);
+        let (tx, rx) = channel::<JobMessage>();
+        let rx = Arc::new(Mutex::new(rx));
+
+        for i in 0..size {
+            pool.push(Worker::new(i,  rx.clone()));
+        }
+
+        return ThreadPool {
+            pool,
+            tx,
+            rx,
+        };
+    }
+    pub fn execute_box(&mut self, f: Box<dyn FnOnce() + Send + 'static>) {
+        self.tx.send(JobMessage::Job(f)).unwrap_or_default();
+    }
+    pub fn execute<F: FnOnce() + Send + 'static>(&mut self, f: F) {
+        self.execute_box(Box::new(f));
+    }
+    fn terminate(&mut self) {
+        for _ in 0..self.pool.len() {
+            self.tx.send(JobMessage::Terminate).unwrap_or_default();
+        }
+        println!("ThreadPool sent {} terminate messages", self.pool.len());
+
+        println!("ThreadPool try drop jobs");
+        // 此处仅用于 所有 worker 都没在等消息, 都在处理 job 时, 消耗所有的 job
+        // 没有消息时, 有一个 worker 会一直持锁, 这边就会死锁, 所以要在前面发 terminate 消息
+        let rx = self.rx.lock().expect("ThreadPool got rx lock error");
+        self.tx.send(JobMessage::Terminate).unwrap_or_default();
+        let mut dropped_count = 0;
+        while let Ok(JobMessage::Job(_)) = rx.recv() { // 最后会消耗一个 teminate, 所以之前要补一个
+            dropped_count += 1;
+        }
+        println!("ThreadPool dropped {} jobs", dropped_count);
+        drop(rx); // rx unlock here
+
+        for worker in &mut self.pool {
+            worker.join();
+        }
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        self.terminate();
+    }
+}
+
+enum JobMessage {
+    Job(Box<dyn FnOnce() + Send + 'static>),
+    Terminate,
+}
+
+struct Worker {
+    id: usize,
+    thread: Option<thread::JoinHandle<()>>,
+}
+
+impl Worker {
+    fn new(id: usize, rx: Arc<Mutex<Receiver<JobMessage>>>) -> Worker {
+        let thread = thread::spawn(move || {
+            loop {
+                // worker 有3种状态
+                // 1. 等锁 0..n
+                let rx = rx.lock().unwrap();
+                // 2. 持锁 0..=1 等消息 没有消息时会一直持锁
+                let msg = rx.recv();
+                // drop(rx); // 解锁 由 NLL 处理
+                // 3. 处理消息 0..=n
+                match msg {
+                    Ok(JobMessage::Job(job)) => {
+                        println!("Worker {} got job", id);
+                        job();
+                    }
+                    Ok(JobMessage::Terminate) => {
+                        println!("Worker {} terminating", id);
+                        break;
+                    }
+                    Err(_) => {
+                        println!("Worker {} terminating unexpected", id);
+                        break;
+                    }
+                }
+            }
+        });
+        return Worker {
+            id,
+            thread: Some(thread),
+        };
+    }
+
+    fn join(&mut self) {
+        if let Some(thread) = self.thread.take() {
+            println!("Worker {} waiting for terminating", self.id);
+            thread.join().unwrap_or_default();
+            println!("Worker {} terminated", self.id);
+        }
+    }
+}
